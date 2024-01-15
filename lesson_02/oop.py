b9b7e3abd5e614c39dd9f6231acbfa35cc008e9c
John Doe <john@email.com>
2024-01-15 19:57:48 +0200

    Price class is added

--- lesson_02/oop.py
+++ lesson_02/oop.py
@@ -2,3 +2,18 @@ class Person:
     def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
+
+
+class Price:
+    def __init__(self, currency, value) -> None:
+        self.currency = currency
+        self.value = value
+
+    # TODO: Complete this class
+
+# phone = 1000 USD
+# food = 100 UAH
+# total = phone + food
+
+# 1000 + (100 / 39) = 1000 + 2.56410256 USD
+# 1000 * 39 + 100 = ...

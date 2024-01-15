8b2dc2a5bb12a9d896abdc25c94c7d9ec908c4b6
John Doe <john@email.com>
2024-01-15 20:45:16 +0200

    Type annotations are added

--- lesson_02/oop.py
+++ lesson_02/oop.py
@@ -5,11 +5,12 @@ class Person:
 
 
 class Price:
-    def __init__(self, currency, value) -> None:
-        self.currency = currency
-        self.value = value
+    def __init__(self, currency: str, value: int) -> None:
+        self.currency: str = currency
+        self.value: int = value
 
     # TODO: Complete this class
+
 
 # phone = 1000 USD
 # food = 100 UAH

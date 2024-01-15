1757147b6a1bcad1efd75301d5e982fcfdb6d0de
Marry Doe <marry@email.com>
2024-01-15 19:58:14 +0200

    Food and Phone classes are added

--- lesson_02/oop.py
+++ lesson_02/oop.py
@@ -2,3 +2,13 @@ class Person:
     def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
+
+
+class Food:
+    currency = "UAH"
+    value = 100
+
+
+class Phone:
+    currency = "USD"
+    value = 1000
